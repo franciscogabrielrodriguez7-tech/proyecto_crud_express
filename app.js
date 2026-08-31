6ed1174d682be6be48d9c6675f5811b791f69cfc
gabrielrodriguez <franciscogabrielrodriguez7-tech>
2026-08-31 09:58:02 -0500

    envio de datos y prueba en liteCllient

--- app.js
+++ app.js
@@ -3,14 +3,42 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3003;
-
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({ mensaje: '¡API Rest Full con express!' });
 });
 
+app.get('/api/aprendices', (req, res) => {
+    res.status(200).json({ mensaje: 'Lista de aprendices' });
+});
+
+app.post('/api/aprendices', (req, res) => {
+     const datosAprendiz = req.body
+    res.status(201).json({ mensaje: 'Aprendiz creado', datos: datosAprendiz});
+});
+
+app.patch('/api/aprendices/:id_aprendiz', (req, res) => {
+    res.status(200).json({ mensaje: 'Aprendiz actualizado'});
+});
+
+app.delete('/api/aprendices/:id_aprendiz', (req, res) => {
+    res.status(200).json({ mensaje: 'Eliminar aprendiz' });
+});
+
+app.post('/api/aprendices/login', (req, res) => {
+    const datosAprendiz = req.body;
+    const edad = req.body.edad;
+    if (!datosAprendiz.edad){
+        res.status(400).json({mensaje:'No se recibieron datos del aprendiz'});
+    }
+    else if (edad >= 18) {
+    res.status(201).json({ mensaje: 'Bienvenido', datos: datosAprendiz.nombre, edad: edad });}
+    else {
+        res.status(401).json({mensaje:'No puedes ingresar, eres menor de edad', edad: edad});
+    }
+});
+
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
 });
