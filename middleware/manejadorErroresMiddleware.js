7cdaa4bef3a56521302c556f2b5aef53f15830b7
gabrielrodriguez <franciscogabrielrodriguez7-tech>
2026-09-14 10:26:16 -0500

    configuración de manejador de errores y de dotenv

--- middleware/manejadorErroresMiddleware.js
+++ middleware/manejadorErroresMiddleware.js
@@ -1,6 +1,15 @@
-const manejadorErroresMiddleware = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ mensaje: 'Error interno del servidor' });
+const manejadorErroresMiddleware = (error, req, res, next) => {
+    const codigoError = error.statusCode || 500;
+    const mensajeError = error.message || 'Error interno del servidor';
+    console.error(`[Error]: ${new Date().toISOString()} - ${codigoError} - ${mensajeError} - ${req.method} ${req.url} - ${req.ip}`);
+    if (error.stack){
+        console.log(error.stack)
+    }
+    res.json({ERROR: "", codigoError, mensajeError,
+// configurar .env, para mostrar errores solo en modo development
+    ...codigoError(process.env.NODE_ENV === 'development', { stack: error.stack })
+    })
+    next()
 }
 
 module.exports = manejadorErroresMiddleware;
